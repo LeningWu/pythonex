70464cd68497ccb8446099c9d28d9bfe140d947d
LeningWu <wulening@lenovo-PC-3.local>
2018-10-10 15:15:59 +0800

    Initial commit

---
+++ c1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env/python
+#!-*-coding:utf-8 -*-
+#!@Time     :2018/10/10  11:37
+#!@Author   :Lening Wu
+#!@File     c1.py
+
+print("hello1213")
+print("jahdjahdjahd")
+print("hjdjshdjkshdkja")
